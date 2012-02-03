cd6d1dbfd761a79e30ae2202c729705f6d2bb421
Mehdi Dogguy <mehdi@debian.org>
2012-02-03 10:00:06 +0100

    Exécuter l'exemple pour chaque runtime

--- rml/examples/async/test.sh
+++ rml/examples/async/test.sh
@@ -5,6 +5,11 @@ if [ "$1" = "-v" ]; then
   verbose='y'
   shift
 fi
+testrun=
+if [ "$1" = "-run" ]; then
+  testrun='y'
+  shift
+fi
 
 runtimes="Lco_rewrite Lco_ctrl_tree Lco_ctrl_tree_n Lco_ctrl_tree_class Lk"
 if [ -n "$1" ]; then
@@ -15,12 +20,14 @@ for run in $runtimes; do
   echo "============================================"
   echo "Testing runtime $run…"
   echo "============================================"
-  ../../compiler/rmlc -runtime $run async1.rml
-  ocamlc -thread -rectypes -I ../../interpreter unix.cma threads.cma rmllib.cma async1.ml -o /dev/null 2>&1
+  rm -f async1.ml async1
+  ../../compiler/rmlc -s main -runtime $run async1.rml
+  ocamlc -thread -rectypes -I ../../interpreter unix.cma threads.cma rmllib.cma async1.ml -o async1 2>&1
   if [ $? -ne 0 ]; then
     [ -n "$verbose" ] && cat -n async1.ml
   else
-    echo "Ok!"
+    echo "Ok!";
+    [ -n "$testrun" ] && why-cpulimit 3 ./async1
   fi
   echo
 done
