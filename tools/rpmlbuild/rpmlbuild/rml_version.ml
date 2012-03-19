3265328316f192be153d9b658d989556a5563fa2
Cédric Pasteur <cedric.pasteur@ens.fr>
2012-03-19 09:39:44 +0100

    This file should not be in the repository

--- tools/rpmlbuild/rpmlbuild/rml_version.ml
+++
@@ -1,8 +0,0 @@
-open Command
-
-let rmlc = A"/Users/ccpasteur/Documents/work/git/rml/compiler/rpmlc.byte";;
-let rmldep = A "/Users/ccpasteur/Documents/work/git/rml/tools/rpmldep/rpmldep.byte";;
-
-let rpmllib_dir = "/Users/ccpasteur/Documents/work/git/rml/interpreter/_build"
-
-let mpicc = "openmpicc"
