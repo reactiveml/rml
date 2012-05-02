0d8e669f77767fd51e4a156d07aaf051d957c9b5
Mehdi Dogguy <mehdi@debian.org>
2012-05-02 16:08:30 +0200

    embedrzi: do not generate a file if something failed

--- rml/configure-tools/embedrzi.ml
+++ rml/configure-tools/embedrzi.ml
@@ -1,21 +1,27 @@
 
+let (//) = Filename.concat;;
+
 let basedir = Array.get Sys.argv ((Array.length Sys.argv) -1) in
-let out = open_out_gen
-  [Open_creat; Open_wronly; Open_trunc]
-  0o640
-  (Filename.concat (Filename.concat (Sys.getcwd()) "global") "rzi.ml")
-in
-let () = Printf.fprintf out "let known_modules = [\n" in
+let stdlib = basedir // "stdlib" in
+let buf = Buffer.create 2048 in
+let () = Buffer.add_string buf "let known_modules = [\n" in
 let () = Array.iter
   (fun file ->
     if Filename.check_suffix file ".rzi" then
-    let fh = open_in (Filename.concat basedir file) in
+    let fh = open_in (stdlib // file) in
     let md = (input_value fh : Def_modules.module0) in
     let modname = String.uncapitalize (Filename.chop_suffix file ".rzi") in
-    Printf.fprintf out " \"%s\", \"%s\";\n"
-      modname
-      (String.escaped (Marshal.to_string md []))
+    Buffer.add_string buf
+      (Printf.sprintf " \"%s\", \"%s\";\n"
+        modname
+        (String.escaped (Marshal.to_string md [])))
   )
-  (Sys.readdir basedir)
+  (Sys.readdir stdlib)
 in
-Printf.fprintf out "]\n"
+let () = Buffer.add_string buf "]" in
+let out = open_out_gen
+  [Open_creat; Open_wronly; Open_trunc]
+  0o640
+  (basedir // "compiler" // "global" // "rzi.ml")
+in
+Printf.fprintf out "%s\n" (Buffer.contents buf)
