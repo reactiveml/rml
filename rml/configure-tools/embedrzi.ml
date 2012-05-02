6103b0e070715bc367c4dd335a4c24766c754aa2
Mehdi Dogguy <mehdi@debian.org>
2012-05-02 17:00:45 +0200

    stdlib/rmltop_*.rmli are not needed
    … well, not entirely. Rmltop_global.rmli is still needed
    but we will read it from its original location (i.e. toplevel
    directory). Rmltop_controller.rmli is not needed at all
    anymore.

--- rml/configure-tools/embedrzi.ml
+++ rml/configure-tools/embedrzi.ml
@@ -5,18 +5,27 @@ let basedir = Array.get Sys.argv ((Array.length Sys.argv) -1) in
 let stdlib = basedir // "stdlib" in
 let buf = Buffer.create 2048 in
 let () = Buffer.add_string buf "let known_modules = [\n" in
-let () = Array.iter
+let stdlib_files = Array.to_list (Sys.readdir stdlib) in
+let stdlib_files = List.map (fun f -> stdlib // f) stdlib_files in
+let stdlib_files =
+  let rmltop_global = basedir // "toplevel" // "rmltop_global.rzi" in
+  if Sys.file_exists rmltop_global then
+    rmltop_global :: stdlib_files
+  else
+    stdlib_files
+in
+let () = List.iter
   (fun file ->
     if Filename.check_suffix file ".rzi" then
-    let fh = open_in (stdlib // file) in
+    let fh = open_in file in
     let md = (input_value fh : Def_modules.module0) in
-    let modname = String.uncapitalize (Filename.chop_suffix file ".rzi") in
+    let modname = Filename.basename (String.uncapitalize (Filename.chop_suffix file ".rzi")) in
     Buffer.add_string buf
       (Printf.sprintf " \"%s\", \"%s\";\n"
         modname
         (String.escaped (Marshal.to_string md [])))
   )
-  (Sys.readdir stdlib)
+  stdlib_files
 in
 let () = Buffer.add_string buf "]" in
 let out = open_out_gen
