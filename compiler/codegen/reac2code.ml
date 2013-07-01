6d362665ec4a3e670429de682723b5c262776b20
Cédric Pasteur <cedric.pasteur@ens.fr>
2013-07-01 16:03:12 +0200

    Fixed syntax of generated code

--- compiler/codegen/reac2code.ml
+++ compiler/codegen/reac2code.ml
@@ -67,8 +67,8 @@ let compile_impl info_chan filename module_name intermediate_code =
               "(* "^(Array.fold_right (fun s cmd -> s^" "^cmd) Sys.argv " ")^
               "*)\n\n");
         (* selection of the interpreter *)
-        output_string out_chan ("open "^ !interpreter_impl ^";;\n");
-        output_string out_chan ("module Machine = "^ !machine_module ^ "(Interpreter)");
+        output_string out_chan ("open "^ !interpreter_impl ^"\n");
+        output_string out_chan ("module Machine = "^ !machine_module ^ "(Interpreter);;\n");
 
         (* the implementation *)
         compile_implementation_back_end info_chan out_chan module_name intermediate_code;
