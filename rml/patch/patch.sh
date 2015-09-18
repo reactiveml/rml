747c09c7448d4a48dd05a29576978783eac370e4
Louis Mandel <lmandel@us.ibm.com>
2015-09-17 23:33:16 -0400

    Change the patch script to avoid the "invalid cross device link" on Linux.

--- rml/patch/patch.sh
+++ rml/patch/patch.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
 
 file=$1
-
-if [ -f ../$file ]; then
-    echo "../$file already exists."
+cd ..
+if [ -f $file ]; then
+    echo "$file already exists."
 else
-    if ! patch -o ../$file $file $file.diff; then
+    if ! patch -o $file patch/$file patch/$file.diff; then
 	echo "unable to create ../$file."
 	exit 1
     fi
