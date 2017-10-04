cbb86efb9f14252aaaa920a4bf68963d99d984a7
Louis Mandel <lmandel@us.ibm.com>
2017-10-04 19:56:25 -0400

    Remove rmlbuild from the distribution.
    Now rmlbuild lives as an independent project.

--- rml/tools/rmlbuild/rmlbuild/ocamlbuild_where.ml
+++
@@ -1,7 +0,0 @@
-let bindir = ref Ocamlbuild_Myocamlbuild_config.bindir;;
-let libdir = ref begin
-  Filename.concat
-    (try Sys.getenv "RMLLIB"
-     with Not_found -> Ocamlbuild_Myocamlbuild_config.libdir)
-    "rmlbuild"
-end;;
