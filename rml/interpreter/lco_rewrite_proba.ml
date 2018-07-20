f8ecc0fa72b914cf9b7939917880ae9a78878816
Louis Mandel <lmandel@us.ibm.com>
2018-07-20 10:41:25 -0400

    Add distributions.

--- rml/interpreter/lco_rewrite_proba.ml
+++ rml/interpreter/lco_rewrite_proba.ml
@@ -29,94 +29,6 @@
 (* $Id: lco_rewrite.ml,v 1.1 2005/04/30 16:49:15 mandel Exp $ *)
 
 
-module Distribution = struct
-  type proba = float
-  type log_proba = float
-
-  type 'a t =
-    | Dist_sampler of ((unit -> 'a) * ('a -> log_proba))
-    | Dist_support of ('a * proba) list
-
-  let bernoulli p =
-    assert (0. <= p && p <= 1.);
-    Dist_support [
-      (true, p);
-      (false, 1. -. p);
-    ]
-
-  let gaussian mu sigma =
-    let two_pi = 2.0 *. 3.14159265358979323846 in
-    let rec rand_pair () =
-      let u1 = Random.float 1.0 in
-      let u2 = Random.float 1.0 in
-      if u1 < epsilon_float then rand_pair ()
-      else u1, u2
-    in
-    Dist_sampler
-      ((fun () ->
-          let u1, u2 = rand_pair() in
-          let z = sqrt (-.2. *. log u1) *. cos (two_pi *. u2) in
-          z *. sigma +. mu),
-       (fun x ->
-          -. 0.5 *. log (two_pi *. sigma ** 2.) -.
-          (x -. mu) ** 2. /. (2. *. sigma ** 2.)))
-
-  let draw dist =
-    begin match dist with
-      | Dist_sampler (sampler, _) -> sampler ()
-      | Dist_support sup ->
-        let sample = Random.float 1.0 in
-        (* TODO data structure for more efficient sampling *)
-        let rec draw sum r =
-          begin match r with
-            | [] -> assert false
-            | (v, p) :: r ->
-              let sum = sum +. p in
-              if sample <= sum then v else draw sum r
-          end
-        in
-        draw 0. sup
-    end
-
-  let score dist x =
-    begin match dist with
-      | Dist_sampler (_, scorer) -> scorer x
-      | Dist_support sup -> log (List.assoc x sup)
-    end
-
-  let draw_and_score dist =
-    begin match dist with
-      | Dist_sampler (sampler, scorer) ->
-        let x = sampler () in
-        x, (scorer x)
-      | Dist_support sup ->
-        let sample = Random.float 1.0 in
-        (* TODO data structure for more efficient sampling *)
-        let rec draw sum r =
-          begin match r with
-            | [] -> assert false
-            | (v, p) :: r ->
-              let sum = sum +. p in
-              if sample <= sum then v, (log p) else draw sum r
-          end
-        in
-        draw  0. sup
-    end
-
-  let multivariate dists =
-    Dist_sampler
-      ((fun () -> List.map (fun dist -> draw dist) dists),
-       (fun xs ->
-          List.fold_left2
-            (fun acc dist x -> acc +. ((score dist) x))
-            1.0
-            dists xs))
-
-  let sph_gaussian mus sigmas =
-    multivariate (List.map2 gaussian mus sigmas)
-
-end
-
 module Rml_interpreter (* : Lco_interpreter.S *) =
   functor (Event: Sig_env.S) ->
   struct
@@ -128,24 +40,28 @@ module Rml_interpreter (* : Lco_interpreter.S *) =
 
     and 'a status = SUSP | STOP | TERM of 'a
 
-    and 'a expr = state option -> 'a status * 'a expr
+    and ('a, 'b) expr = 'b state option -> 'a status * ('a, 'b) expr
 
-    and state =
-      { mutable st_resample: bool;
+    and 'b state =
+      { mutable st_proposition: 'b list;
+        mutable st_resample: bool;
         mutable st_score: float;
         st_id: int; }
 
-    and 'a process = unit -> 'a expr
+    and ('a, 'b) model = unit -> ('a, 'b) expr
 
+    and 'a process = ('a, unit) model
 
-(* manupulation de l'état *)
+(* manipulation de l'état *)
     let make_state id =
-      { st_resample = false;
+      { st_proposition = [];
+        st_resample = false;
         st_score = 0.;
         st_id = id; }
 
     let cp_state src id =
-      { st_resample = false;
+      { st_proposition = src.st_proposition;
+        st_resample = false;
         st_score = src.st_score;
         st_id = id; }
 
@@ -475,7 +391,7 @@ module Rml_interpreter (* : Lco_interpreter.S *) =
 (**************************************)
 (* seq                                *)
 (**************************************)
-    let rec rml_seq (p1 : 'a expr) (p2 : 'b expr) : 'b expr =
+    let rec rml_seq p1 p2 =
       fun state ->
         match p1 state with
         | TERM _, _ ->
@@ -1225,8 +1141,19 @@ module Rml_interpreter (* : Lco_interpreter.S *) =
         rml_factor_v score state
 
 (**************************************)
+(* propose                            *)
+(**************************************)
+
+    let rml_propose_v v =
+      fun state ->
+        move := true;
+        state.st_proposition <- v :: state.st_proposition;
+	(TERM (), rml_nothing)
+
+(**************************************)
 (* infer                              *)
 (**************************************)
+
     let nb_particules = 10
 
     let list_init n f =
@@ -1312,7 +1239,7 @@ module Rml_interpreter (* : Lco_interpreter.S *) =
           List.mapi
             (fun i body ->
                match body with
-               | SUSP, p, ({ st_resample = false} as sample_state) ->
+               | SUSP, p, ({ st_resample = false } as sample_state) ->
                  let alpha, p' = p (Some sample_state) in
                  infer_status := gamma !infer_status alpha;
                  alpha, p', sample_state
